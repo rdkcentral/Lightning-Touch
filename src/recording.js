fe4421bb41131eca0460957d67ba3cacdac569f2
erikhaandrikman <e.haandrikman@metrological.com>
2021-11-03 12:55:56 +0100

    Moved finger and recording storage logic

---
+++ src/recording.js
@@ -0,0 +1,67 @@
+/**
+ * Recordings per toucharea
+ * @type {Map}
+ */
+const recordings = new Map();
+
+/**
+ * recordings per area that already have been analyzed
+ * we only store one per area
+ * @type {Map}
+ */
+const analyzed = new Map();
+
+/**
+ * Active fingers touching the surface
+ * @type {Map}
+ */
+const fingers = new Map();
+
+export const hasRecording = (key) => {
+    if(!key){
+        return;
+    }
+    return recordings.has(key);
+};
+
+export const getRecording = (key) => {
+    if (hasRecording(key)) {
+        return recordings.get(key);
+    }
+};
+
+export const addRecording = (key, recording) => {
+    recordings.set(key, recording);
+};
+
+export const removeRecording = (key)=>{
+    if(hasRecording(key)){
+        recordings.delete(key);
+    }
+}
+
+export const hasFinger = (identifier)=>{
+    return fingers.has(identifier);
+}
+
+export const getFinger = (identifier)=>{
+    if(hasFinger(identifier)){
+        return fingers.get(identifier);
+    }
+}
+
+export const addFinger = (identifier, areaId)=>{
+    fingers.set(identifier, areaId);
+}
+
+export const removeFinger = (identifier)=>{
+    fingers.delete(identifier);
+}
+
+export const getFingersOnArea = (areaId)=>{
+    return [...fingers.values()].filter((id)=>id === areaId)
+}
+
+export const getAreaByFingerId = (identifier) => {
+    return getFinger(identifier);
+}
